4ea22194fe05ccf19173a52e6c3355a9c27f3839
Ramy Ghorayeb <ramy.ghorayeb@free.fr>
2016-07-31 00:45:10 +0200

    Database in the Server

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,22 +2,12 @@ function AppCtrl($scope, $http) {
     
     // Contrôle //
     console.log("Controller AppCtrl connected to the view");
+     
+    // Récupération de la database //
+    $http.get('/orderlist').success(function(response) { 
+        // Contrôle //
+        console.log("Le controlleur AppCtrl a bien reçu la database du serveur");
+        $scope.orderlist = response;
     
-    order1 = {
-        number : '#534',
-        drinks : '1 Amine Mojito'
-    };
-    
-    order2 = {
-        number : '#681',
-        drinks : '2 Vodkaaris'
-    };
-    
-    order3 = {
-        number : '#280',
-        drinks : '1 Smirohff'
-    };
-    
-    var orderlist = [order1, order2, order3];
-    $scope.orderlist = orderlist;
+    });
 };
--- server.js
+++ server.js
@@ -10,6 +10,32 @@ var bodyParser=require('body-parser');
 // Liaison entre le serveur et l'app //
 app.use(express.static(__dirname + "/public"));
 
+// Récupération de la database //
+app.get('/orderlist', function(req,res) {
+    
+    // Contrôle //
+    console.log("La database du serveur a été envoyée au controlleur AppCtrl");
+    
+    order1 = {
+        number : '#534',
+        drinks : '1 Amine Mojito'
+    };
+    
+    order2 = {
+        number : '#681',
+        drinks : '2 Vodkaaris'
+    };
+    
+    order3 = {
+        number : '#280',
+        drinks : '1 Smirohff'
+    };
+    
+    var orderlist = [order1, order2, order3];
+    
+    res.json(orderlist);
+});
+
 // Port du serveur //
 app.listen(3000);
 
