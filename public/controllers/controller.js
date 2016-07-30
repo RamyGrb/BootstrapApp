0d62ebaf7954c2385fb855b42195c7c85776d745
Ramy Ghorayeb <ramy.ghorayeb@free.fr>
2016-07-31 00:11:38 +0200

    Database in the Controller

---
+++ public/controllers/controller.js
@@ -0,0 +1,23 @@
+function AppCtrl($scope) {
+    
+    // contrôle //
+    console.log("Controller AppCtrl connected to the view");
+    
+    order1 = {
+        number : '#534',
+        drinks : '1 Amine Mojito'
+    };
+    
+    order2 = {
+        number : '#681',
+        drinks : '2 Vodkaaris'
+    };
+    
+    order3 = {
+        number : '#280',
+        drinks : '1 Smirohff'
+    };
+    
+    var orderlist = [order1, order2, order3];
+    $scope.orderlist = orderlist;
+};
---
+++ server.js
@@ -0,0 +1,18 @@
+// Dépendances //
+var express=require('express');
+var app = express();
+
+var mongojs=require('mongojs');
+var db=mongojs('contactlist', ['contactlist']);
+
+var bodyParser=require('body-parser');
+
+// Liaison entre le serveur et l'app //
+app.use(express.static(__dirname + "/public"));
+
+
+// Port du serveur //
+app.listen(3000);
+
+// Contrôle //
+console.log("Server running on port 3000");
