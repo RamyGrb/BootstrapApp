baff8de4d42d96d5522dfab1886336ce02492c5b
Ramy Ghorayeb <ramy.ghorayeb@free.fr>
2016-08-06 18:39:57 +0200

    Liaison des databases restantes
    Liaison des différentes collections des bdd de mongo

--- public/js/controller.js
+++ public/js/controller.js
@@ -29,17 +29,17 @@ function ClientCtrl($scope, $http) {
     // Contrôle //
     console.log("Controller ClientCtrl connected to the view");
     
-    // Récupération de la database des cocktails //
-    $http.get('/drinklist').success(function(response) {
-        // Contrôle //
-        console.log("Controller ClientCtrl received the drink database from the server");
-        $scope.drinklist = response;
-    });
-    
     // Récupération de la database des commandes //
     $http.get('/orderlist').success(function(response) {
         // Contrôle //
         console.log("Controller ClientCtrl received the order database from the server");
         $scope.orderlist = response;
+    });
+    
+    // Récupération de la database des boissons //
+    $http.get('/drinklist').success(function(response) {
+        // Contrôle //
+        console.log("Controller ClientCtrl received the drink database from the server");
+        $scope.drinklist = response;
     });
 };
--- server.js
+++ server.js
@@ -3,24 +3,43 @@ var express=require('express');
 var app = express();
 
 var mongojs=require('mongojs');
-var db=mongojs('orderlist', ['orderlist']);
+var db=mongojs('orderlist', ['orderlist','drinklist']);
 
 var bodyParser=require('body-parser');
+
+var mongoose = require('mongoose');
 
 // Liaison entre le serveur et l'app //
 app.use(express.static(__dirname + "/public"));
 
-// Récupération de la database //
+// Récupération de la database orderlist //
 app.get('/orderlist', function(req,res) {
     
     // Contrôle //
-    console.log("Server GET request sended to the controller");
+    console.log("Server GET orderlist request");
     
     // Connexion à Mongodb, docs = la database récupérée //
     db.orderlist.find(function(err,docs){
        
         // Contrôle //
-        console.log("Database sended to the controller");
+        console.log("Database orderlist sent to the controller");
+        
+        // Envoi de la database au controleur //
+        res.json(docs);
+    });
+});
+
+// Récupération de la database drinklist //
+app.get('/drinklist', function(req,res) {
+    
+    // Contrôle //
+    console.log("Server GET drinklist request");
+    
+    // Connexion à Mongodb, docs = la database récupérée //
+    db.drinklist.find(function(err,docs){
+       
+        // Contrôle //
+        console.log("Database drinklist sent to the controller");
         
         // Envoi de la database au controleur //
         res.json(docs);
