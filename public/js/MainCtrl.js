aa27e88acc94bce3dfa6bf6bf36fbba9a71c6762
Ramy Ghorayeb <ramy.ghorayeb@free.fr>
2016-08-11 19:03:40 +0200

    Nettoyage et Décoration

---
+++ public/js/MainCtrl.js
@@ -0,0 +1,12 @@
+// Main Contrôleur de l'Application //
+function AppCtrl($scope, $http) {
+    // Contrôle //
+    console.log("Controller AppCtrl connected to the app");
+};
+
+// Contrôleur de l'Acceuil //
+function AcceuilCtrl($scope, $http) {
+    
+    // Contrôle //
+    console.log("Controller AcceuilCtrl connected to the view Acceuil");
+};
