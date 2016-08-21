100900a7c7846a8e35a2d09be26d9774a18b7cdd
Ramy Ghorayeb <ramy.ghorayeb@free.fr>
2016-08-21 02:58:16 +0200

    préparation

--- public/js/app.js
+++ public/js/app.js
@@ -1,40 +1,61 @@
-var BillyApp = angular.module('BillyApp', ['ngRoute']);
+var BillyApp = angular.module('BillyApp', ['ui.router']);
+
+BillyApp.config(function($stateProvider, $urlRouterProvider) {
+
+    // Indique ce qu'on fait en cas de route connue/inconnue //
+    $urlRouterProvider.when('', '/');
+    $urlRouterProvider.otherwise("/error");
+
+    $stateProvider    
+
+        .state('splash', {
+            url: "/",
+            templateUrl: "../views/splash.html",
+        })
+
+        .state('login', {
+            url: "/login",
+            templateUrl: "../views/login.html",
+        })
+
+        .state('signup', {
+            url: "/signup",
+            templateUrl: "../views/signup.html",
+        })  
+
+        .state('home', {
+            url: "/home",
+            templateUrl: "../views/home.html",
+        })
     
- BillyApp.config(['$routeProvider', function($routeProvider) {
-        
-     $routeProvider
+        .state('home.accueil', {
+            url: "/accueil",
+            templateUrl: "../views/accueil.html",
+            controller: "AccueilCtrl"        
+        })
+    
+        .state('home.bar', {
+            url: "/bar",
+            templateUrl: "../views/bar.html",
+            controller: "BarCtrl"        
+        })
+    
+        .state('home.client', {
+            url: "/client",
+            templateUrl: "../views/client.html",
+            controller: "ClientCtrl"
+        })
+    
+        .state('home.account', {
+            url: "/account",
+            templateUrl: "../views/account.html",
+            controller: "AccountCtrl"
+        })    
 
-         .when('/', {
-                templateUrl : '../views/accueil.html',
-                controller  : 'AccueilCtrl'
-            })
-     
-         .when('/login', {
-                templateUrl : '../views/login.html',
-                controller  : 'LoginCtrl'
-            })     
-
-         .when('/signup', {
-                templateUrl : '../views/signup.html',
-                controller  : 'SignupCtrl'
-            })
-
-         .when('/bar', {
-                templateUrl : '../views/bar.html',
-                controller  : 'BarCtrl'
-            })
-     
-         .when('/client', {
-                templateUrl : '../views/client.html',
-                controller  : 'ClientCtrl'
-            })  
-     
-         .when('/account', {
-                templateUrl : '../views/account.html',
-                controller  : 'AccountCtrl'
-            }) 
-     
-         .otherwise({
-            templateUrl: '../views/error.html'
-            });
- }]);
+        .state('error', {
+            url: "/error",
+            templateUrl: "../views/error.html",
+        })
+    
+    
+});
